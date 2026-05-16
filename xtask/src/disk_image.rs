e2f8c07b99d66500933ac66e9ac25928408859d5
Pierre Krieger <pierre.krieger1708@gmail.com>
2026-05-16 17:06:48 +0200

    Tweaks

--- xtask/src/disk_image.rs
+++ xtask/src/disk_image.rs
@@ -5,38 +5,69 @@ pub fn build_disk_image(
     init_program_path: impl AsRef<Path>,
     disk_image_path: impl AsRef<Path>,
 ) -> Result<(), io::Error> {
-    let mut file = fs::File::create(&disk_image_path)?;
+    let mut file = fs::OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open(disk_image_path)?;
     file.set_len(512 * 1024 * 1024)?;
+
+    let partition_size = {
+        let mut disk = gpt::GptConfig::new()
+            .writable(true)
+            .create_from_device(&mut file, None)
+            .unwrap(); // TODO: don't panic; convert error
+
+        let start_sector = 2048;
+        let (_, last_usable_sector) = disk
+            .find_free_sectors()
+            .first()
+            .copied()
+            .ok_or_else(|| io::Error::other("No free sectors available"))?;
+
+        disk.add_partition(
+            "boot",
+            (last_usable_sector - start_sector + 1) * 512,
+            gpt::partition_types::EFI,
+            0,
+            None,
+        )
+        .unwrap(); // TODO: don't panic; convert error
+        disk.write().unwrap(); // TODO: don't panic; convert error
+
+        (last_usable_sector - start_sector + 1) * 512
+    };
+
+    {
+        // TODO: better offset calculation
+        let mut partition_slice =
+            fscommon::StreamSlice::new(&mut file, 2048 * 512, 2048 * 512 + partition_size)?;
+
+        fatfs::format_volume(
+            &mut partition_slice,
+            // TODO: volume label
+            fatfs::FormatVolumeOptions::new().fat_type(fatfs::FatType::Fat32),
+        )?;
+
+        {
+            let fs = fatfs::FileSystem::new(&mut partition_slice, fatfs::FsOptions::new())?;
+
+            let root_dir = fs.root_dir();
+
+            let mut kernel_writer = root_dir
+                .create_dir("EFI")?
+                .create_dir("BOOT")?
+                .create_file("BOOTX64.EFI")?;
+            io::copy(&mut fs::File::open(kernel_file_path)?, &mut kernel_writer)?;
+
+            let mut initramfs_writer = root_dir.create_file("initramfs.cpio.gz")?;
+            let init_program = fs::read(init_program_path)?;
+            io::Write::write_all(&mut initramfs_writer, &initramfs(&init_program))?;
+        }
+    }
+
     io::Write::flush(&mut file)?;
-    drop(file);
-
-    let mut buf_stream = fscommon::BufStream::new(
-        fs::OpenOptions::new()
-            .read(true)
-            .write(true)
-            .open(disk_image_path)?,
-    );
-
-    fatfs::format_volume(
-        &mut buf_stream,
-        // TODO: volume label
-        fatfs::FormatVolumeOptions::new().fat_type(fatfs::FatType::Fat32),
-    )?;
-
-    let fs = fatfs::FileSystem::new(buf_stream, fatfs::FsOptions::new())?;
-
-    let root_dir = fs.root_dir();
-
-    let mut kernel_writer = root_dir
-        .create_dir("EFI")?
-        .create_dir("BOOT")?
-        .create_file("BOOTX64.EFI")?;
-    io::copy(&mut fs::File::open(kernel_file_path)?, &mut kernel_writer)?;
-
-    let mut initramfs_writer = root_dir.create_file("initramfs.cpio.gz")?;
-    let init_program = fs::read(init_program_path)?;
-    io::Write::write_all(&mut initramfs_writer, &initramfs(&init_program))?;
-
     Ok(())
 }
 
